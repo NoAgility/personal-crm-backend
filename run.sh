2e25b378b2b6a51c69a377388961de2adf06aa2a
Lucien Lu <lucien.lu@student.unimelb.edu.au>
2021-08-27 22:46:57 +1000

    Transferred some jobs from run.sh to setup.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 cd /home/ec2-user/personal-crm-backend
-docker-compose --env-file /home/ec2-user/personal-crm-backend/config/prod.env build --no-cache
 docker-compose --env-file /home/ec2-user/personal-crm-backend/config/prod.env up -d
