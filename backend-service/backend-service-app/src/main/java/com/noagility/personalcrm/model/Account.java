186e81f50e3916d4d657a4fc937b7ef180f47e09
Orochac <O.Rochanakij@protonmail.com>
2021-09-02 18:36:00 +1000

    Create Account.java

---
+++ backend-service/backend-service-app/src/main/java/com/noagility/personalcrm/model/Account.java
@@ -0,0 +1,77 @@
+package com.noagility.personalcrm.model;
+
+import java.time.LocalDate;
+
+public class Account {
+    private int AccountID;
+    private int RoleID;
+    private String AccountName;
+    private LocalDate AccountDOB;
+    private LocalDate AccountCreation;
+
+    public Account(int accountID, int roleID, String accountName, LocalDate accountDOB, LocalDate accountCreation) {
+        AccountID = accountID;
+        RoleID = roleID;
+        AccountName = accountName;
+        AccountDOB = accountDOB;
+        AccountCreation = accountCreation;
+    }
+
+    public Account(int roleID, String accountName, LocalDate accountDOB, LocalDate accountCreation) {
+        RoleID = roleID;
+        AccountName = accountName;
+        AccountDOB = accountDOB;
+        AccountCreation = accountCreation;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Account{" +
+                "AccountID=" + AccountID +
+                ", RoleID=" + RoleID +
+                ", AccountName='" + AccountName + '\'' +
+                ", AccountDOB=" + AccountDOB +
+                ", AccountCreation=" + AccountCreation +
+                '}';
+    }
+
+    public int getAccountID() {
+        return AccountID;
+    }
+
+    public int getRoleID() {
+        return RoleID;
+    }
+
+    public String getAccountName() {
+        return AccountName;
+    }
+
+    public LocalDate getAccountDOB() {
+        return AccountDOB;
+    }
+
+    public LocalDate getAccountCreation() {
+        return AccountCreation;
+    }
+
+    public void setAccountID(int accountID) {
+        AccountID = accountID;
+    }
+
+    public void setRoleID(int roleID) {
+        RoleID = roleID;
+    }
+
+    public void setAccountName(String accountName) {
+        AccountName = accountName;
+    }
+
+    public void setAccountDOB(LocalDate accountDOB) {
+        AccountDOB = accountDOB;
+    }
+
+    public void setAccountCreation(LocalDate accountCreation) {
+        AccountCreation = accountCreation;
+    }
+}
