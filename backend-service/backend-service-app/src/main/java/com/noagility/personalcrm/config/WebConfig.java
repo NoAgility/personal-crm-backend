13cd7e10e4bde38b4887f2d328f91ea4fd9a0966
Lucien Lu <lucien.lu@student.unimelb.edu.au>
2021-09-17 15:41:27 +1000

    Fixed autowire bean bug

--- backend-service/backend-service-app/src/main/java/com/noagility/personalcrm/config/WebConfig.java
+++ backend-service/backend-service-app/src/main/java/com/noagility/personalcrm/config/WebConfig.java
@@ -1,12 +1,11 @@
 package com.noagility.personalcrm.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
