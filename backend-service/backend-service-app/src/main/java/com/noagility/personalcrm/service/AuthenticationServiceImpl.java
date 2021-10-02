3031338f84da01e112d2894ab952f90d975f893a
nate3223 <nathanice3223@gmail.com>
2021-10-02 16:02:52 +1000

    yes

--- backend-service/backend-service-app/src/main/java/com/noagility/personalcrm/service/AuthenticationServiceImpl.java
+++ backend-service/backend-service-app/src/main/java/com/noagility/personalcrm/service/AuthenticationServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 
 
