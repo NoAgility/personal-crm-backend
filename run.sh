674ff8de4d69eb2fe1647639d73bed30870844ab
lucienl100 <68990764+lucienl100@users.noreply.github.com>
2021-10-04 10:27:58 +1100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/ec2-user/personal-crm-backend
-docker-compose --file docker-compose-springboot.yml --env-file config/dev.env up -d
+docker-compose --file docker-compose-springboot.yml --env-file config/prod.env up -d
